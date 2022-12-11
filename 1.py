f430e396e47712802407363dffd8fe509ae1548c
Ignaty88 <ignaty88@gmail.com>
2022-12-12 00:00:48 +0300

    test

---
+++ 1.py
@@ -0,0 +1,10 @@
+Python 3.8.10 (v3.8.10:3d8993a744, May  3 2021, 09:09:08) 
+[Clang 12.0.5 (clang-1205.0.22.9)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello Liza")
+Hello Liza
+>>> 
+KeyboardInterrupt
+>>> print("Hello Liza")
+Hello Liza
+>>> 
